04119a0c3efffe920dd831a097c303c5b27a20e2
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-03-31 12:36:11 +0900

    BMI
    2022-03-31
    BMI계산기 클론코딩

---
+++ AopPart2Chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
@@ -0,0 +1,36 @@
+package fastcampus.aop.part2.chapter01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val heightEditText : EditText = findViewById(R.id.heightEditText)
+        val weightEditText : EditText = findViewById(R.id.weightEditText)
+        val confirmButton : Button = findViewById(R.id.confirm_button)
+
+        confirmButton.setOnClickListener{
+            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val height : Int = heightEditText.text.toString().toInt()
+            val weight : Int = weightEditText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+
+            startActivity(intent)
+        }
+    }
+}
---
+++ AopPart2Chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/ResultActivity.kt
@@ -0,0 +1,32 @@
+package fastcampus.aop.part2.chapter01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlin.math.pow
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        val bmi = weight / (height / 100.0).pow(2.0)
+        val resultText = when{
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5-> "정상체중"
+            else -> "저체중"
+        }
+
+        val resultValueTextView = findViewById<TextView>(R.id.bmiResultTextView)
+        val resultStringTextView = findViewById<TextView>(R.id.ResultTextView)
+
+        resultValueTextView.text = " $bmi"
+        resultStringTextView.text = " $resultText"
+    }
+}
