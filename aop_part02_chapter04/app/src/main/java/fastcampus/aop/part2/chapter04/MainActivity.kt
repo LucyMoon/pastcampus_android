0dc8ebaa5a2e94794e5dd98e731073870b1fab0c
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-04-07 09:45:20 +0900

    Calculator
    2022-04-07
    계산기 계산 기능 클론코딩

--- aop_part02_chapter04/app/src/main/java/fastcampus/aop/part2/chapter04/MainActivity.kt
+++ aop_part02_chapter04/app/src/main/java/fastcampus/aop/part2/chapter04/MainActivity.kt
@@ -2,27 +2,168 @@ package fastcampus.aop.part2.chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private var isOperator = false
+    private var hasOperator = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View){
-
+    fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+        }
     }
 
-    fun resultButtonClicked(v: View){
+    private fun numberButtonClicked(number: String) {
+        if (isOperator) {
+            expressionTextView.append(" ")
+        }
+        isOperator = false
 
+        val expressionText = expressionTextView.text.split(" ")
+        if (expressionText.isNotEmpty() && expressionText.last().length > 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+            return
+        } else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        expressionTextView.append(number)
+        resultTextView.text = calculateExpression()
     }
 
-    fun historyButtonClicked(v: View){
+    private fun operatorButtonClicked(operator: String) {
+        if (expressionTextView.text.isEmpty()) {
+            return
+        }
+        when {
+            isOperator -> {
+                val text = expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) + operator
+            }
+            hasOperator -> {
+                Toast.makeText(this, "연산자는 한 번만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+            else -> {
+                expressionTextView.append(" $operator")
+            }
+        }
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            expressionTextView.text.length - 1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        expressionTextView.text = ssb
 
+        isOperator = true
+        hasOperator = true
     }
 
-    fun clearButtonClicked(v: View){
+    fun resultButtonClicked(v: View) {
+        val expressionTexts = expressionTextView.text.split(" ")
 
+        if(expressionTextView.text.isEmpty() || expressionTexts.size == 1){
+            return
+        }
+
+        if(expressionTexts.size != 3 && hasOperator){
+            Toast.makeText(this, "아직 완성되지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()){
+            Toast.makeText(this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val expressionText = expressionTextView.text.toString()
+        val resultText = calculateExpression()
+
+        resultTextView.text = ""
+        expressionTextView.text = resultText
+
+        isOperator = false
+        hasOperator = false
+    }
+
+    private fun calculateExpression(): String {
+        val expressionTexts = expressionTextView.text.split(" ")
+
+        if (hasOperator.not() || expressionTexts.size != 3) {
+            return ""
+        } else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            return ""
+        }
+
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
+
+        return when (op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "*" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
+    }
+
+    fun clearButtonClicked(v: View) {
+        expressionTextView.text = ""
+        resultTextView.text = ""
+        isOperator = false
+        hasOperator = false
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+}
+
+fun String.isNumber(): Boolean {
+    return try {
+        this.toBigInteger()
+        true
+    } catch (e: NumberFormatException) {
+        false
     }
 }
