5cb66abf150e13899243e429446c32668be11602
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-06-19 17:28:03 +0900

    pomodoro
    2022-06-19
    뽀모도로 타이머

---
+++ aoppart2chapter6/app/src/main/java/fastcampus/aop/part2/chapter06/MainActivity.kt
@@ -0,0 +1,131 @@
+package fastcampus.aop.part2.chapter06
+
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    private val soundPool = SoundPool.Builder().build()
+
+    private var currentCountDownTimer: CountDownTimer? = null
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindView()
+        initSounds()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+
+    private fun bindView() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    if(p2) {
+                        updateRemainTimes(p1 * 60 * 1000L)
+                    }
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                    stopCountDown()
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                    p0 ?: return //좌측에 있는 값이 null일경우 우측에 값을 리턴
+
+                    if(seekBar.progress == 0){
+                        stopCountDown()
+                    }else {
+                        startCountDown()
+                    }
+                }
+            }
+        )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+        object : CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(p0: Long) {
+                updateRemainTimes(p0)
+                updateSeekBar(p0)
+            }
+
+            override fun onFinish() {
+                completeCountDown()
+            }
+        }
+
+    private fun startCountDown() {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun stopCountDown(){
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown(){
+        updateRemainTimes(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
+    }
+
+    private fun updateRemainTimes(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+
+    private fun initSounds(){
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+}
